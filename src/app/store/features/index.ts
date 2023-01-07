0115b6cc64768ae9a910e0bac561ac030c23f663
or2008 <or2008@gmail.com>
2023-01-08 00:01:14 +0200

    remove unused code

--- src/app/store/features/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as themeAtom } from './theme'
--- src/app/store/features/theme.ts
+++
@@ -1,6 +0,0 @@
-import { Theme } from '@my-app'
-import { atomWithStorage } from 'jotai/utils'
-
-const themeAtom = atomWithStorage<Theme>('APP_THEME', 'dark')
-
-export default themeAtom
